da7c7feb934d39cada3b94aeb3fceac67cdfceaa
DragonSinMeliodas <bansall.shubham@gmail.com>
2016-02-21 00:01:06 +0530

    SignUp Page added and Intents added Switching Completed

--- app/src/main/java/com/example/shubham/chato_fy/MainActivity.java
+++ app/src/main/java/com/example/shubham/chato_fy/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.shubham.chato_fy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void goToSignUpActivity(View view){
+        Intent i = new Intent(this, SignUpActivity.class);
+        startActivity(i);
+    }
+    public void gotoForgotPasswordActivity(View view){
+        Intent i = new Intent(this, ForgotPasswordActivity.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/example/shubham/chato_fy/SignUpActivity.java
@@ -0,0 +1,21 @@
+package com.example.shubham.chato_fy;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+
+    public void gotoMainActivity(View view){
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
+
+}

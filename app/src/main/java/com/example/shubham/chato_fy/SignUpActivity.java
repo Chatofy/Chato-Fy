5b67ab8f11487316a155034d29d4c801a59d9561
DragonSinMeliodas <bansall.shubham@gmail.com>
2016-02-21 23:28:56 +0530

    password check popup added

--- app/src/main/java/com/example/shubham/chato_fy/SignUpActivity.java
+++ app/src/main/java/com/example/shubham/chato_fy/SignUpActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -19,5 +21,19 @@ public class SignUpActivity extends AppCompatActivity {
     }
 
     public void checkRegister(View view) {
+
+        EditText rollNumberField = (EditText)findViewById(R.id.rollNumberField);
+        EditText passwordField = (EditText)findViewById(R.id.passwordField);
+        EditText repeatPasswordField = (EditText)findViewById(R.id.repeatPasswordField);
+
+        String rollNumberFieldStr = rollNumberField.getText().toString();
+        String passwordFieldStr = passwordField.getText().toString();
+        String repeatPasswordFieldStr = repeatPasswordField.getText().toString();
+
+        if(!passwordFieldStr.equals(repeatPasswordFieldStr)){
+
+            Toast passError = Toast.makeText(this, "Passwords Don't Match!", Toast.LENGTH_LONG);
+            passError.show();
+        }
     }
 }
